1699ecad2502ca152f6dc9513fc0cc81f6af2c57
Stéphane Morel <110528810+Steph4477@users.noreply.github.com>
2023-10-19 13:08:56 +0200

    add ls and string builder

--- Cli.java
+++ Cli.java
@@ -1,5 +1,6 @@
 import java.util.Map;
 import java.util.Scanner;
+import java.io.File;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.LocalDateTime;
@@ -9,43 +10,75 @@ public class Cli {
         Scanner scanner = new Scanner(System.in);
         System.out.print("> "); // Invite
         while (true) {
-            String commandLine = scanner.nextLine();
-            String[] commandTable = commandLine.split(" ", 2); // Split la ligne de commande
-            String command = commandTable[0]; 
+            String scanLine = scanner.nextLine();
+            String[] commandTable = scanLine.split(" ", 2); // Split la ligne de commande
+            String commandName = commandTable[0];
             String arguments = (commandTable.length > 1) ? commandTable[1] : "";
-            String output = ""; // Variable pour stocker la sortie
-            String lineSeparator = System.getProperty("line.separator");
+            String output = "";
 
-            if (command.equals("exit") || command.equals("logout")) {
+            if (commandName.equals("exit") || commandName.equals("logout")) {
                 System.out.println("Bye !");
                 scanner.close();
-                return;
-            } else if (command.equals("date")) {
+                break;
+
+            } else if (commandName.equals("date")) {
                 output = LocalDate.now().toString();
-            } else if (command.equals("time")) {
+            } else if (commandName.equals("time")) {
                 output = LocalTime.now().toString();
-            } else if (command.equals("datetime")) {
+            } else if (commandName.equals("datetime")) {
                 output = LocalDateTime.now().toString();
-            } else if (command.equals("useraccount")) {
+            } else if (commandName.equals("useraccount")) {
                 output = System.getProperty("user.name");
-            } else if (command.equals("userhome")) {
+            } else if (commandName.equals("userhome")) {
                 output = System.getProperty("user.home");
-            } else if (command.equals("os")) {
-                output = System.getProperty("os.name") + " (" + System.getProperty("os.version") + ")";
-            } else if (command.equals("printenv")) {
+            } else if (commandName.equals("os")) {
+                StringBuilder buildOs = new StringBuilder();
+                buildOs
+                        .append(System.getProperty("os.name"))
+                        .append(" (")
+                        .append(System.getProperty("os.version"))
+                        .append(")");
+                output = buildOs.toString();
+            } else if (commandName.equals("printenv")) {
                 if (!arguments.isEmpty()) {
                     String value = System.getenv(arguments);
-                    output = value;
+                    output = (value != null) ? value : "command not found";
                 } else {
+                    StringBuilder buildPrintenv = new StringBuilder();
                     Map<String, String> env = System.getenv();
                     for (Map.Entry<String, String> entry : env.entrySet()) {
-                        output += entry.getKey() + "=" + entry.getValue() + lineSeparator;
+                        buildPrintenv
+                                .append(entry.getKey())
+                                .append("=")
+                                .append(entry.getValue())
+                                .append(System.lineSeparator());
                     }
+                    output = buildPrintenv.toString();
                 }
-            } else if (command.equals("echo") || command.equals("print")) {
+            } else if (commandName.equals("echo") || commandName.equals("print")) {
                 output = arguments;
+            } else if (commandName.equals("ls")) {
+                File folder = new File(arguments);
+                if (folder.isDirectory()) {
+                    StringBuilder buildLs = new StringBuilder();
+                    File[] files = folder.listFiles();
+                    for (File file : files) {
+                        buildLs
+                                .append(file.getName())
+                                .append(System.lineSeparator());
+                    }
+                    output = buildLs.toString();
+                } else {
+                    output = "Not a directory";
+                }
+
             } else {
-                output = "Command '" + command + "' not found.";
+                StringBuilder cnf = new StringBuilder();
+                cnf
+                        .append("Command '")
+                        .append(commandName)
+                        .append("' not found.");
+                output = cnf.toString();
             }
             System.out.println(output);
             System.out.print("> "); // Invite
